d952e8157ef12d8c2b54e2f0bc49711231711662
LittleStupid <lttlstpd@live.com>
2016-08-17 20:49:23 +0800

    item can be created

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,10 +1,20 @@
-$("li").click( function(){
+$("ul").on( "click", "li", function(){
   $(this).toggleClass( "completed" );
 });
 
-$("span").click( function(event){
+$("ul").on( "click", "span", function(event){
   $(this).parent().fadeOut( 500, function(){
     $(this).remove();
   });
   event.stopPropagation();
+});
+
+$("input[type='text']").keypress( function(event){
+  //create new item if enter key is pressed.
+  if( event.which === 13 ){
+    var todoText = $(this).val();
+
+    $(this).val("");
+    $("ul").append("<li><span>X</span> " + todoText + "</li>");
+  }
 });
