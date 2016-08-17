37aa0a0a9af04446c60c14cbabc0d5e79943e669
LittleStupid <lttlstpd@live.com>
2016-08-17 20:06:21 +0800

    refactor : toggle css Class when item is clicked

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,14 +1,3 @@
 $("li").click( function(){
-  if($(this).css("color") === "rgb(128, 128, 128)" ){ //gray?
-    $(this).css( {
-      color: "black",
-      textDecoration: "none"
-    } );
-  }
-  else{
-    $(this).css( {
-      color: "gray",
-      textDecoration: "line-through"
-    } );
-  }
+  $(this).toggleClass( "completed" );
 });
